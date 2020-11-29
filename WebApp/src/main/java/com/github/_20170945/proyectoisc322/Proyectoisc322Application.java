ceae27d9f2cea6cdd9c0c99d1b547b98b0d6eeeb
Zhen P. Li L <20170945@ce.pucmm.edu.do>
2020-11-29 18:57:04 -0400

    Init.

---
+++ WebApp/src/main/java/com/github/_20170945/proyectoisc322/Proyectoisc322Application.java
@@ -0,0 +1,13 @@
+package com.github._20170945.proyectoisc322;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectoisc322Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyectoisc322Application.class, args);
+    }
+
+}
---
+++ WebApp/src/test/java/com/github/_20170945/proyectoisc322/Proyectoisc322ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github._20170945.proyectoisc322;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectoisc322ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
